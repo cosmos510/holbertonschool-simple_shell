3d142c5ae9e7ce18ec07606e5125b7706a54f639
cosmos510 <maximemartin510@gmail.com>
2024-04-16 08:27:39 +0000

    Add header file

---
+++ s_shell.h
@@ -0,0 +1,12 @@
+#ifndef S_SIMPLE_H
+#define S_SIMPLE_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+#endif
