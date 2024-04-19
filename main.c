05d6ef0d9a15c77e29282f52167b6de1df791e4e
cosmos510 <maximemartin510@gmail.com>
2024-04-19 14:28:02 +0000

    dealing with non interactif

--- main.c
+++ main.c
@@ -5,16 +5,34 @@
  * @av: list of arguments
  * Return: 0 if program run with success
  */
-
-int main(int ac, char **av)
+int main(int ac, __attribute__((unused)) char **av)
 {
-    if (ac == 1 && strcmp(av[0], "./hsh") == 0)
-    {
-        run_prompt();
-    }
-    else if (ac > 1)
-    {
-        perror("hsh ");
-    }
-    return (0);
+	ssize_t bytes_read;
+	size_t bufsize = 1024;
+	if (ac == 1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			run_prompt();
+		}
+		else
+		{
+			bytes_read = getline(&test, &bufsize, stdin);
+			if (bytes_read == -1)
+			{
+				free(test);
+				exit(1);
+			}
+			if (test[bytes_read - 1] == '\n')
+				test[bytes_read - 1] = '\0';
+
+			read_line(test);
+		}
+
+	}
+	else
+	{
+		perror("hsh:");
+	}
+	return (0);
 }
