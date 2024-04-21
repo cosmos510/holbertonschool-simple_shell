1fe0248e53272069072c9eb9229d2bd82ca713ea
cosmos510 <maximemartin510@gmail.com>
2024-04-21 12:02:05 +0000

    updating prototype of getenv

--- shell.h
+++ shell.h
@@ -19,9 +19,8 @@ int read_line(char *command);
 void new_env(char *executable_path, char *args[]);
 void _printev(void);
 int *tokenize_path(char **args_command);
-char *_getenv(char *var);
+char *_getenv(const char *var);
 void execute_command(char **args, char **args_command);
-void new_env(char *executable_path, char *args[]);
 void signal_callback_handler(int x);
 void our_exit(int status);
 #endif
