ba396a63cc93924e527a647f0d35b47b96c20e17
cosmos510 <maximemartin510@gmail.com>
2024-04-23 12:16:21 +0000

    bouble while for getline

--- promt_no_in.c
+++ promt_no_in.c
@@ -1,26 +1,35 @@
+
 #include "shell.h"
 
 
 int prompt_no_inter()
 {
 	int bytes_read =0;
-	bytes_read = _getline();
 
-		if (test[bytes_read - 1] == '\n')
-			test[bytes_read - 1] = '\0';
+	while(1)
+	{
+		bytes_read = _getline();
+		if (bytes_read > 0)
+		{
+			if (test[bytes_read - 1] == '\n')
+				test[bytes_read - 1] = '\0';
 
-		if (strcmp(test, "exit") == 0)
-		{
-			free(test);
-			exit(0);
+			if (strcmp(test, "exit") == 0)
+			{
+				free(test);
+				exit(0);
+			}
+			if (strcmp(test, "env") == 0)
+				_printev(environ);
+			else
+			{
+				read_line(test);
+
+			}
 		}
-		if (strcmp(test, "env") == 0)
-			_printev(environ);
-		else
-		{
-			read_line(test);
-			return(0);
-		}
+
+	}
+
 
 
 	return (0);
