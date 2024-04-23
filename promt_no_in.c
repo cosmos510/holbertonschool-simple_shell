738092fbff843fc6a7aa1fd38b95341ee6e8a949
cosmos510 <maximemartin510@gmail.com>
2024-04-23 08:00:25 +0000

    back to normal

--- promt_no_in.c
+++ promt_no_in.c
@@ -3,23 +3,10 @@
 
 int prompt_no_inter()
 {
-	int is_whitespace = 1;
-	int i = 0;
 	int bytes_read =0;
-
 	bytes_read = _getline();
-	is_whitespace = 1;
-		for (i = 0; i < bytes_read; i++)
-		{
-			if (test[i] != ' ' && test[i] != '\n' && test[i] != '\t')
-			{
-				is_whitespace = 0;
-				break;
-			}
-		}
-		if (is_whitespace)
-		{
-			if (test[bytes_read - 1] == '\n')
+
+		if (test[bytes_read - 1] == '\n')
 			test[bytes_read - 1] = '\0';
 
 		if (strcmp(test, "exit") == 0)
@@ -34,8 +21,6 @@ int prompt_no_inter()
 			read_line(test);
 			return(0);
 		}
-		}
-
 
 
 	return (0);
